a4b15814a5b7a67c2c5a082636d47aa8c046352a
vladkhodan <pva.vapr@mail.ru>
2017-02-13 12:00:15 +0300

    function

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 var url = require('./url.js');
 console.log("app.js -- запущен");
 console.log("Создан 3_commit");
+console.error("Error");
+var phraze = {	Hello: "Привет", world: "мир"};
+
+function PhrazeName(name)
+{
+	return phraze[name];
+}
+console.log("%s , %s!", PhrazeName("Hello"), PhrazeName("world"));
